18188a66371483be42cd6fc16a477658286a9b20
陈宝迪 <chenbaodi2@h-partners.com>
2024-08-01 20:08:26 +0800

    fix:修改turboModule中的接口，适配源库接口类型
    Signed-off-by: 陈宝迪 <chenbaodi2@h-partners.com>

--- harmony/amap_geolocation/src/main/ets/AMapGeolocationModule.ts
+++ harmony/amap_geolocation/src/main/ets/AMapGeolocationModule.ts
@@ -22,91 +22,91 @@
  * SOFTWARE.
  */
 
-import { TurboModule, TurboModuleContext } from "@rnoh/react-native-openharmony/ts";
-import hilog from '@ohos.hilog';
+import { TurboModule, TurboModuleContext } from '@rnoh/react-native-openharmony/ts';
 import { TM } from '@rnoh/react-native-openharmony/generated/ts';
 import {
-  IAMapLocationListener,
+  AMapLocation,
   AMapLocationManagerImpl,
   AMapLocationOption,
-  AMapLocation
+  AMapLocationReGeocodeLanguage,
+  AMapLocationType,
+  AMapPrivacyAgreeStatus,
+  AMapPrivacyInfoStatus,
+  AMapPrivacyShowStatus,
+  e45,
+  IAMapLocationListener
 } from '../../../IndexMap';
 
-import abilityAccessCtrl, { PermissionRequestResult } from '@ohos.abilityAccessCtrl';
+import abilityAccessCtrl, { PermissionRequestResult, Permissions } from '@ohos.abilityAccessCtrl';
 import common from '@ohos.app.ability.common';
-import { AMapPrivacyShowStatus, AMapPrivacyInfoStatus, AMapPrivacyAgreeStatus, } from '../../../IndexMap';
 
-import { AMapLocationReGeocodeLanguage } from '../../../IndexMap';
-
-import { CommonConstants } from './CommonConstants'
+import { CommonConstants } from './CommonConstants';
 import geoLocationManager from '@ohos.geoLocationManager';
-import systemDateTime from '@ohos.systemDateTime';
-import { AMapLocationType } from '../../../IndexMap';
 import bundleManager from '@ohos.bundle.bundleManager';
-import { Permissions } from '@ohos.abilityAccessCtrl';
-import { e45 } from '../../../IndexMap';
-
-import { BusinessError } from '@ohos.base';
-import { Context } from '@kit.AbilityKit';
 
 export class AMapGeolocationModule extends TurboModule implements IAMapLocationListener, TM.RNAMapGeolocation.Spec {
   private options: AMapLocationOption | null = null;
   private client: AMapLocationManagerImpl | null = null;
-  private context: TurboModuleContext | null = null;
-  private reGeocodeLanguage: AMapLocationReGeocodeLanguage = AMapLocationReGeocodeLanguage.Chinese;
-  private interval: number = 5;
-  private result: string = ''
-  private errorMessage: string = ''
+  private isSingle: boolean = false;
 
   constructor(ctx: TurboModuleContext) {
     super(ctx);
-
-    hilog.debug(0xcc00, '[RNOH]', ':FileUpLoadModule constructor');
     this.options = {
       priority: geoLocationManager.LocationRequestPriority.FIRST_FIX,
       scenario: geoLocationManager.LocationRequestScenario.UNSET,
       maxAccuracy: 0,
-      singleLocationTimeout: 3000,
+      timeInterval: 2,
+      singleLocationTimeout: 10000,
       allowsBackgroundLocationUpdates: false,
-      locatingWithReGeocode: false,
+      locatingWithReGeocode: true,
+      distanceInterval: 0,
       reGeocodeLanguage: AMapLocationReGeocodeLanguage.Chinese,
       isOffset: false
     }
-
   }
-
   onLocationChanged(location: AMapLocation): void {
-    this.result = JSON.stringify(location);
     this.ctx.rnInstance.emitDeviceEvent(
       "AMapGeolocation",
-      this.toJSON(location)
+      {
+        timestamp: location.timeStamp,
+        accuracy: location.accuracy,
+        latitude: location.latitude,
+        longitude: location.longitude,
+        altitude: location.altitude,
+        speed: location.speed,
+        direction: location.direction,
+        timeSinceBoot: location.timeSinceBoot,
+        additions: location.additions,
+        additionSize: location.additionSize,
+        isOffset: location.isOffset,
+        reGeo: location.reGeo,
+        address: location.reGeo?.address,
+        description: location.reGeo?.desc,
+        poiName: location.reGeo?.poiName,
+        country: location.reGeo?.country,
+        province: location.reGeo?.province,
+        city: location.reGeo?.city,
+        cityCode: location.reGeo?.citycode,
+        district: location.reGeo?.district,
+        street: location.reGeo?.street,
+        adCode: location.reGeo?.adcode
+      }
     )
   };
 
   onLocationError(locationErrorInfo: e45): void {
-    this.errorMessage = JSON.stringify(locationErrorInfo);
   };
 
   requestPermissions(): void {
     let atManager = abilityAccessCtrl.createAtManager();
     try {
-
       atManager.requestPermissionsFromUser(this.ctx.uiAbilityContext, CommonConstants.REQUEST_PERMISSIONS)
         .then((data) => {
-          console.error('AMapGeolocation requestPermissionsFromUser')
-
           if (data.authResults[0] !== 0 || data.authResults[1] !== 0) {
-            console.error('AMapGeolocation requestPermissionsFromUser return')
             return;
           }
-          const that = this;
-
-        })
-        .catch((err: Error) => {
-          console.error('AMapGeolocation requestPermissionsFromUser err' + JSON.stringify(err))
         })
     } catch (err) {
-      console.error('AMapGeolocation requestPermissionsFromUser err' + JSON.stringify(err));
     }
   }
 
@@ -122,16 +122,12 @@ export class AMapGeolocationModule extends TurboModule implements IAMapLocationL
       let appInfo: bundleManager.ApplicationInfo = bundleInfo.appInfo;
       tokenId = appInfo.accessTokenId;
     } catch (error) {
-      let err: BusinessError = error as BusinessError;
-      console.error(`AMapGeolocation Failed to get bundle info for self. Code is ${err.code}, message is ${err.message}`);
     }
 
     // 校验应用是否被授予权限
     try {
       grantStatus = await atManager.checkAccessToken(tokenId, permission);
     } catch (error) {
-      let err: BusinessError = error as BusinessError;
-      console.error(`AMapGeolocation Failed to check access token. Code is ${err.code}, message is ${err.message}`);
     }
 
     return grantStatus;
@@ -144,9 +140,6 @@ export class AMapGeolocationModule extends TurboModule implements IAMapLocationL
     if (grantStatus === abilityAccessCtrl.GrantStatus.PERMISSION_GRANTED) {
       // 已经授权，可以继续访问目标操作
     } else {
-      // 申请日历权限
-      const permissions: Array<Permissions> = ['ohos.permission.LOCATION'];
-
       this.reqPermissionsFromUser(CommonConstants.REQUEST_PERMISSIONS);
     }
   }
@@ -163,86 +156,77 @@ export class AMapGeolocationModule extends TurboModule implements IAMapLocationL
           // 用户授权，可以继续访问目标操作
         } else {
           // 用户拒绝授权，提示用户必须授权才能访问当前页面的功能，并引导用户到系统设置中打开相应的权限
-          // this.openPermissionsInSystemSettings()
           return;
         }
       }
-      // 授权成功
-    }).catch((err: BusinessError) => {
-      console.error(`Failed to request permissions from user. Code is ${err.code}, message is ${err.message}`);
     })
   }
 
-  init(key: string) {
-    let context: common.UIAbilityContext = this.ctx.uiAbilityContext;
-    this.checkPermissions();
-    if (this.client != null) {
-      this.client.stopContinuousTask();
-    }
-    AMapLocationManagerImpl.setApiKey(key);
-    AMapLocationManagerImpl.updatePrivacyShow(AMapPrivacyShowStatus.DidShow, AMapPrivacyInfoStatus.DidContain, context);
-    AMapLocationManagerImpl.updatePrivacyAgree(AMapPrivacyAgreeStatus.DidAgree, context);
-    if (this.client == null) {
-      this.client = new AMapLocationManagerImpl(context);
+  init(key: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let context: common.UIAbilityContext = this.ctx.uiAbilityContext;
+        this.checkPermissions();
+        if (this.client != null) {
+          this.client.stopContinuousTask();
+        }
+        AMapLocationManagerImpl.setApiKey(key);
+        AMapLocationManagerImpl.updatePrivacyShow(AMapPrivacyShowStatus.DidShow, AMapPrivacyInfoStatus.DidContain,
+          context);
+        AMapLocationManagerImpl.updatePrivacyAgree(AMapPrivacyAgreeStatus.DidAgree, context);
 
-    }
-    this.client?.setLocationListener(AMapLocationType.Single, this)
-  }
-
-  start(): Promise<string> {
-    this.client?.setLocationListener(AMapLocationType.Updating, this)
-    this.client?.setLocationOption(AMapLocationType.Updating, this.options)
-    this.client?.startUpdatingLocation()
-    return new Promise((resolve, reject) => {
-
-      if (this.result != '') {
-        resolve(this.result)
-      } else {
-        reject(this.errorMessage)
+        if (this.client == null) {
+          this.client = new AMapLocationManagerImpl(context);
+        }
+        this.client?.setLocationListener(AMapLocationType.Single, this)
+        resolve();
+      } catch (exception) {
+        reject(exception)
       }
     })
+  }
+
+  start(): void {
+    if (this.isSingle) {
+      this.client?.setLocationListener(AMapLocationType.Single, this)
+      this.client?.setLocationOption(AMapLocationType.Single, this.options)
+      this.client?.requestSingleLocation()
+    } else {
+      this.client?.setLocationListener(AMapLocationType.Updating, this)
+      this.client?.setLocationOption(AMapLocationType.Updating, this.options)
+      this.client?.startUpdatingLocation()
+    }
   }
 
   stop(): void {
     this.client?.stopUpdatingLocation()
   }
 
-
-  getLastKnownLocation(): Promise<string> {
-    this.client?.setLocationOption(AMapLocationType.Last, this.options);
-    this.client?.setLocationListener(AMapLocationType.Last, this);
-    this.client?.requestLastLocation();
-    return new Promise((resolve, reject) => {
-      if (this.result != '') {
-        resolve(this.result)
-      } else {
-        reject(this.errorMessage)
-      }
-    });
-  }
-
-  setOnceLocation(): Promise<string> {
-    this.client?.setLocationListener(AMapLocationType.Single, this)
-    this.client?.setLocationOption(AMapLocationType.Single, this.options)
-    this.client?.requestSingleLocation()
-    return new Promise((resolve, reject) => {
-      if (this.result != '') {
-        resolve(this.result)
-      } else {
-        reject(this.errorMessage)
-      }
-    })
+  setOnceLocation(onceLocation: boolean): void {
+    if (onceLocation) {
+      this.isSingle = true;
+    } else {
+      this.isSingle = false;
+    }
   }
 
   setInterval(interval: number): void {
     if (this.options != null) {
-      this.options.timeInterval = interval;
+      this.options.timeInterval = interval / 1000;
     }
   }
 
-  setGeoLanguage(language: number): void {
+  setGeoLanguage(language: TM.RNAMapGeolocation.GeoLanguage): void {
+    let realLanguage;
     if (this.options != null) {
-      this.options.reGeocodeLanguage = language;
+      if (language == TM.RNAMapGeolocation.GeoLanguage.ZH) {
+        realLanguage = AMapLocationReGeocodeLanguage.Chinese;
+      } else if (language == TM.RNAMapGeolocation.GeoLanguage.EN) {
+        realLanguage = AMapLocationReGeocodeLanguage.English;
+      } else {
+        realLanguage = AMapLocationReGeocodeLanguage.Default;
+      }
+      this.options.reGeocodeLanguage = realLanguage;
     }
   }
 
@@ -275,36 +259,5 @@ export class AMapGeolocationModule extends TurboModule implements IAMapLocationL
       this.options.maxAccuracy = desiredAccuracy;
     }
   }
-
-  private toJSON(location: AMapLocation): Map<string, ESObject> {
-    if (location == null) {
-      return new Map<string, ESObject>();
-    }
-    let map: Map<string, ESObject> = new Map<string, ESObject>();
-    map.set("timestamp", location.timeStamp);
-    map.set("accuracy", location.accuracy);
-    map.set("latitude", location.latitude);
-    map.set("longitude", location.longitude);
-    map.set("altitude", location.altitude);
-    map.set("speed", location.speed);
-    map.set("direction", location.direction);
-    map.set("timeSinceBoot", location.timeSinceBoot);
-    map.set("additions", location.additions);
-    map.set("additionSize", location.additionSize);
-    map.set("isOffset", location.isOffset);
-    map.set("reGeo", location.reGeo);
-    if (location.reGeo != undefined) {
-      map.set("address", location?.reGeo.address);
-      map.set("description", location.reGeo.desc);
-      map.set("poiName", location.reGeo.poiName);
-      map.set("country", location.reGeo.country);
-      map.set("province", location.reGeo.province);
-      map.set("city", location.reGeo.city);
-      map.set("cityCode", location.reGeo.citycode);
-      map.set("district", location.reGeo.district);
-      map.set("street", location.reGeo.street);
-      map.set("adCode", location.reGeo.adcode);
-    }
-    return map;
-  }
 }
+
